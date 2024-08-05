e4a552b996eaab2211c26122c503a81bd7e0c52e
Gaurang Telang <gstelang@gmail.com>
2024-08-05 09:20:33 -0700

    experimenting with fuzz

--- other/rle.go
+++ other/rle.go
@@ -2,7 +2,66 @@ package other
 
 import (
 	"strconv"
+	"unicode"
 )
+
+func isDigit(r rune) bool {
+	return unicode.IsDigit(r)
+}
+
+func isASCII(s string) bool {
+	for i := 0; i < len(s); i++ {
+		if s[i] > 127 {
+			return false
+		}
+	}
+	return true
+}
+
+func decodeRle(input string) string {
+	if len(input) == 0 {
+		return input
+	}
+
+	if len(input) == 1 {
+		return input
+	}
+
+	if !isASCII(input) {
+		return input
+	}
+
+	output := ""
+	multiplyCount := 1
+	var prevChar rune
+
+	for _, char := range input {
+		if string(char) == "0" || isDigit(char) && isDigit(prevChar) {
+			return input
+		}
+		if isDigit(char) {
+			multiplyCount, _ = strconv.Atoi(string(char))
+		} else {
+			for i := 0; i < multiplyCount; i++ {
+				output += string(char)
+			}
+		}
+		prevChar = char
+	}
+
+	return output
+}
+
+func isValid(input string) bool {
+	isValid := false
+	for _, char := range input {
+		isValid = (char >= 65 && char <= 90) || (char >= 97 && char <= 122)
+		if !isValid {
+			return false
+		}
+	}
+	return isValid
+}
 
 func rle(input string) string {
 
@@ -10,18 +69,25 @@ func rle(input string) string {
 		return ""
 	}
 
-	if len(input) == 1 {
-		return "1" + string(input[0])
+	if !isValid(input) {
+		return input
 	}
 
-	const maxChars = 9
+	if len(input) == 1 {
+		return input
+	}
+
+	const maxCharacterCount = 9
 	output := ""
 	prevChar := string(input[0])
 	currentCount := 1
 
 	for i := 1; i < len(input); i++ {
+
 		currentChar := string(input[i])
-		if currentChar == prevChar {
+		// for loop gives you byte. Convert to rune
+		// range gives you rune.
+		if currentChar == prevChar && currentCount < maxCharacterCount {
 			currentCount++
 		} else {
 			output += strconv.Itoa(currentCount) + prevChar
--- other/rle_test.go
+++ other/rle_test.go
@@ -18,3 +18,39 @@ func TestAllSame(t *testing.T) {
 		t.Errorf("failed to match all same chars")
 	}
 }
+
+func TestExceedSameCharLimit(t *testing.T) {
+	output := rle("aaaaaaaaaa")
+	if output != "9a1a" {
+		t.Errorf("failed to accomodate max char limit")
+	}
+}
+
+func TestDecode(t *testing.T) {
+	output := decodeRle("1a1b1c1d1e")
+	if output != "abcde" {
+		t.Errorf("Unable to decode")
+	}
+}
+
+//	go test -v ./other/rle.go ./other/rle_test.go -fuzz=Fuzz
+//
+// TODO: need to add more checks with respect allowable chars for this to work correctly.
+func FuzzRle(f *testing.F) {
+	// Seed corpus
+	testcases := []string{"", "a", "aa", "aaa", "aaaaaaaaaa", "abcde", "b", "aaaaa", ""}
+	for _, tc := range testcases {
+		// Provide seed inputs
+		f.Add(tc)
+	}
+
+	f.Fuzz(func(t *testing.T, orig string) {
+		encoded := rle(orig)
+		decoded := decodeRle(encoded)
+
+		if decoded != orig {
+			t.Errorf("rle(%q) = %q, decode(%q) = %q", orig, encoded, encoded, decoded)
+		}
+
+	})
+}
