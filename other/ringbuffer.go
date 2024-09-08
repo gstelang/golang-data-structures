832c89f9ff61ed7839ec8d12d70104a608c9d440
Gaurang Telang <gstelang@gmail.com>
2024-09-08 14:30:14 -0700

    make it generic

--- other/ringbuffer.go
+++ other/ringbuffer.go
@@ -2,35 +2,35 @@ package main
 
 import "fmt"
 
-type RingBuffer struct {
-	logs         []int
+type RingBuffer[T any] struct {
+	logs         []T
 	totalEntries int
 	capacity     int
 }
 
-type RingOperations interface {
-	Add(int)
-	GetRecentEntries() []int
+type RingOperations[T any] interface {
+	Add(T)
+	GetRecentEntries() []T
 }
 
-func (rb *RingBuffer) Add(val int) {
+func (rb *RingBuffer[T]) Add(val T) {
 	nextIndex := rb.totalEntries % rb.capacity
 	rb.logs[nextIndex] = val
 	rb.totalEntries++
 }
 
-func NewRingBuffer(capacity int) *RingBuffer {
-	return &RingBuffer{
-		logs:         make([]int, capacity),
+func NewRingBuffer[T any](capacity int) *RingBuffer[T] {
+	return &RingBuffer[T]{
+		logs:         make([]T, capacity),
 		capacity:     capacity,
 		totalEntries: 0,
 	}
 }
 
-func (rb *RingBuffer) GetRecentEntries() []int {
+func (rb *RingBuffer[T]) GetRecentEntries() []T {
 
 	size := min(rb.totalEntries, rb.capacity)
-	result := make([]int, size)
+	result := make([]T, size)
 
 	index := rb.totalEntries % rb.capacity
 	resultIndex := 0
@@ -52,16 +52,37 @@ func (rb *RingBuffer) GetRecentEntries() []int {
 
 func main() {
 
-	ringBuffer := NewRingBuffer(5)
-
-	ringBuffer.Add(1)
-	ringBuffer.Add(2)
-	ringBuffer.Add(3)
-	ringBuffer.Add(4)
-	ringBuffer.Add(5)
-	ringBuffer.Add(6)
-	ringBuffer.Add(7)
-
-	recent := ringBuffer.GetRecentEntries()
+	// int
+	ringBufferInt := NewRingBuffer[int](5)
+	ringBufferInt.Add(1)
+	ringBufferInt.Add(2)
+	ringBufferInt.Add(3)
+	ringBufferInt.Add(4)
+	ringBufferInt.Add(5)
+	ringBufferInt.Add(6)
+	ringBufferInt.Add(7)
+	recent := ringBufferInt.GetRecentEntries()
 	fmt.Println(recent)
+
+	// str
+	ringBufferStr := NewRingBuffer[string](5)
+	ringBufferStr.Add("foo")
+	ringBufferStr.Add("bar")
+	ringBufferStr.Add("baz")
+	recentStr := ringBufferStr.GetRecentEntries()
+	fmt.Println(recentStr)
+
+	// bytes
+	byteBuffer := NewRingBuffer[[]byte](5)
+	byteBuffer.Add([]byte("ABC"))
+	byteBuffer.Add([]byte("DEF"))
+	byteBuffer.Add([]byte("GHI"))
+	byteBuffer.Add([]byte("JKL"))
+	byteBuffer.Add([]byte("MNO"))
+	byteBuffer.Add([]byte("PQR"))
+	byteBuffer.Add([]byte("STU"))
+
+	for _, bytes := range byteBuffer.GetRecentEntries() {
+		fmt.Println(string(bytes))
+	}
 }
